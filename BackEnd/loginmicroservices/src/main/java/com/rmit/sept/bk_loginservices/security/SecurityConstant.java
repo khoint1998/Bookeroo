8e8fb95b8afdcda5e51a2dd2cf3c3f031e7c1b70
Khoi <s3678755@rmit.edu.vn>
2021-08-18 21:54:10 +0700

    Expanded the token duration to 1 day

--- BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/security/SecurityConstant.java
+++ BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/security/SecurityConstant.java
@@ -7,5 +7,5 @@ public class SecurityConstant {
     public static final String SECRET ="SecretKeyToGenJWTs";
     public static final String TOKEN_PREFIX= "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 30_000; //30 seconds
+    public static final long EXPIRATION_TIME = 864000_000; //30 seconds
 }

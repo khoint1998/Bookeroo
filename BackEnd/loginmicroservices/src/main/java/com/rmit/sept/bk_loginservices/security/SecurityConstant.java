9e4f9ee1b0b640351dcfd9480bd8021524420da4
Khoi <s3678755@rmit.edu.vn>
2021-08-20 10:11:19 +0700

    setup: Fixed Security constant for accessing h2-console

--- BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/security/SecurityConstant.java
+++ BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/security/SecurityConstant.java
@@ -3,7 +3,7 @@ package com.rmit.sept.bk_loginservices.security;
 public class SecurityConstant {
 
     public static final String SIGN_UP_URLS = "/api/users/**";
-    public static final String H2_URL = "h2-console/**";
+    public static final String H2_URL = "/h2-console/**";
     public static final String SECRET ="SecretKeyToGenJWTs";
     public static final String TOKEN_PREFIX= "Bearer ";
     public static final String HEADER_STRING = "Authorization";

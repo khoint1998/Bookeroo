343e763d554988eb933bbb3b55675dfac62d675b
Khoi <s3678755@rmit.edu.vn>
2021-08-22 18:49:44 +0700

    new/be-login-signup: Sample for Frontend: Changed the form and used Formik, applied with custom styling in Register.css

--- FrontEnd/myfirstapp/src/components/UserManagement/Register/Register.js
+++ FrontEnd/myfirstapp/src/components/UserManagement/Register/Register.js
@@ -3,102 +3,99 @@ import { createNewUser } from "../../../actions/securityActions";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import classnames from "classnames";
+import { Formik, Field, Form } from 'formik';
+import Button from '@material-ui/core/Button';
+import './Register.css';
 
-class Register extends Component {
-    constructor(){
-        super();
+const Register = () => {
 
-    this.state = {
-      username: "",
-      fullName: "",
-      password: "",
-      confirmPassword: "",
-      errors: {}
-    };
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
+  // Error Checking
+  // const[error, setError] = null;
+  // componentWillReceiveProps(nextProps){
+  //     if (nextProps.errors){
+  //         this.setState ({
+  //             errors: nextProps.errors
+  //         });
 
-  componentWillReceiveProps(nextProps){
-      if (nextProps.errors){
-          this.setState ({
-              errors: nextProps.errors
-          });
+  //     }
+  // }
 
-      }
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
+  const onSubmit = (values) => {
     const newUser = {
-      username: this.state.username,
-      fullName: this.state.fullName,
-      password: this.state.password,
-      confirmPassword: this.state.confirmPassword
+      username: values.username,
+      email: values.email,
+      fullName: values.fullName,
+      password: values.password,
+      confirmPassword: values.confirmPassword,
+      role: values.role
     };
-
+    alert("sub " + JSON.stringify(newUser, null, 2));
     this.props.createNewUser(newUser, this.props.history);
   }
 
-  onChange(e) {
+  const onChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
-    }
-  render() {
-      const { errors } = this.state;
-    return (
-      <div className="register">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 m-auto">
-              <h1 className="display-4 text-center">Sign Up</h1>
-              <p className="lead text-center">Create your Account</p>
-              <form action="create-profile.html">
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className= {classnames("form-control form-control-lg", {
-                        "is-invalid": errors.name
-                    }) }
-                    placeholder="Name"
-                    name="name"
-                    value= {this.state.name}
-                    required
-                  />
-                  {errors.name && (
-                      <div className= "invalid-feedback">{errors.name}</div>
-                  )}
-                </div>
-                <div className="form-group">
-                  <input
-                    type="email"
-                    className="form-control form-control-lg"
-                    placeholder="Email Address"
-                    name="email"
-                  />
-                </div>
-                <div className="form-group">
-                  <input
-                    type="password"
-                    className="form-control form-control-lg"
-                    placeholder="Password"
-                    name="password"
-                  />
-                </div>
-                <div className="form-group">
-                  <input
-                    type="password"
-                    className="form-control form-control-lg"
-                    placeholder="Confirm Password"
-                    name="password2"
-                  />
-                </div>
-                <input type="submit" className="btn btn-info btn-block mt-4" />
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
   }
+
+  return (
+    <Formik
+    initialValues={{
+      username: '',
+      email: '',
+      fullName: '',
+      password: '',
+      confirmPassword: '',
+      role: ''
+    }}
+    onSubmit = {
+      async (values) => {
+        // await new Promise((r) => setTimeout(r, 500));
+        
+        onSubmit(values);
+      }
+    }
+    >
+    <Form>
+      <label>User Name</label>
+      <Field id="username" name="username" placeholder="Jane" />
+
+      <label>Email:</label>
+      <Field
+        id="email"
+        name="email"
+        placeholder="jane@bookeroo.com"
+        type="email"
+      />
+
+      <label>Full Name:</label>
+      <Field id="fullName" name="fullName" placeholder="Jane Doe" />
+
+      <label>Password:</label>
+      <Field id="password" name="password" />
+
+      <label>Confirm Password:</label>
+      <Field id="confirmPassword" name="confirmPassword" />
+
+      <div id="my-radio-group">I am a</div>
+      <div role="group" aria-labelledby="my-radio-group">
+        <label>
+          <Field type="radio" name="role" value="P" />
+          Publisher
+        </label>
+        <label>
+          <Field type="radio" name="role" value="SO" />
+          Shop owner
+        </label>
+        <label>
+          <Field type="radio" name="role" value="A" />
+          Admin
+        </label>
+      </div>
+      
+      <Button className="submitBtn" type="submit">Submit</Button>
+
+    </Form>
+  </Formik>
+  );
 }
 export default Register;

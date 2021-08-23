8cd6fa32da2c04b26be005c86e50fc2472275de7
Khoi <s3678755@rmit.edu.vn>
2021-08-23 20:58:29 +0700

    new/chore/be-login-signup: Completed styling for Register and Login

--- FrontEnd/myfirstapp/src/components/UserManagement/Login/Login.js
+++ FrontEnd/myfirstapp/src/components/UserManagement/Login/Login.js
@@ -3,49 +3,84 @@ import { Formik, Field, Form } from 'formik';
 import Button from '@material-ui/core/Button';
 import './Login.css';
 import axios from "axios";
+import logo from "../../../pics/logo.png";
+import bookstore from "../../../pics/bookstore.png";
 
 const Login = () => {
 
   const loginAsUser = async (user) => {
     let req = await axios.post('http://localhost:8080/bookeroo/users/login', user)
         .catch(error => error.response.data);
-        console.log(req.data);
     return req.data;
   }
 
   const onSubmit = (values) => {
-    const newUser = {
+    const user = {
       username: values.username,
       password: values.password,
     };
-    loginAsUser(newUser);
+    loginAsUser(user);
   }
 
-  return(
-    <Formik
-      initialValues={{
-        username: '',
-        password: '',
-      }}
-      onSubmit = {
-        async (values) => {
-          onSubmit(values);
-        }
-      }
-    >
-      <Form>
-        <label>User Name</label>
-        <Field id="username" name="username" placeholder="Jane" />
+  return (
+    <div className="background">
+      <div className="form">
+        <div className="picture-slogan">
+          <p className="slogan">Your best bookstore in the <span className="catchy-word">town</span></p>
+          <div className="logo-align"><img className="bookstore-logo" src={bookstore} alt="bookstore"/></div>
+        </div>
+        <div className="registration-form">
+          <div className="logo-align"><img className="logo" src={logo} alt="logo"/></div>
+          <div className="login-form">
+            <Formik
+              initialValues={{
+                username: '',
+                password: '',
+              }}
+              onSubmit = {
+                async (values) => {
+                  onSubmit(values);
+                }
+              }
+            >
+              <Form>
+                <div className="inputFields">
+                  <div>
+                    <div className="align-label-and-field">
+                      <label className="label">User Name:</label>
+                      <Field className="fields" id="username" name="username" />
+                    </div>
 
-        <label>Password:</label>
-        <Field id="password" name="password" />
+                    <div className="align-label-and-field">
+                      <label className="label">Password:</label>
+                      <Field className="fields" id="password" name="password" />
+                    </div>
+                  </div>
         
-        <Button className="loginBtn" type="submit">Login</Button>
+                  <div className="alignBtn">
+                    <Button 
+                      className="submitBtn" 
+                      style={{ 
+                        border: '1px solid #06f', 
+                        borderRadius:'3%',
+                        outline: 'none'
+                      }} 
+                      type="submit"
+                    >
+                      <span className="btn-text">Login</span>
+                    </Button>
+                  </div>
+                  
+                </div>
 
-      </Form>
-    </Formik>
-  );
-               
+              </Form>
+            </Formik>
+          </div>
+          <div className="end-quote">Shop your favourite books, all in one place</div>
+        </div>
+      </div>
+    </div>
+  );             
 }
 
 export default Login;
--- FrontEnd/myfirstapp/src/components/UserManagement/Register/Register.js
+++ FrontEnd/myfirstapp/src/components/UserManagement/Register/Register.js
@@ -3,13 +3,14 @@ import { Formik, Field, Form } from 'formik';
 import Button from '@material-ui/core/Button';
 import './Register.css';
 import axios from "axios";
+import logo from "../../../pics/logo.png";
+import bookstore from "../../../pics/bookstore.png";
 
 const Register = () => {
 
   const creatUser = async (newUser) => {
     let req = await axios.post('http://localhost:8080/bookeroo/users/register', newUser)
         .catch(error => error.response.data);
-        console.log(req.data);
     return req.data;
   }
 
@@ -26,62 +27,103 @@ const Register = () => {
   }
 
   return (
-    <Formik
-      initialValues={{
-        username: '',
-        email: '',
-        fullName: '',
-        password: '',
-        confirmPassword: '',
-        role: ''
-      }}
-      onSubmit = {
-        async (values) => {
-          onSubmit(values);
-        }
-      }
-    >
-      <Form>
-        <label>User Name</label>
-        <Field id="username" name="username" placeholder="Jane" />
-
-        <label>Email:</label>
-        <Field
-          id="email"
-          name="email"
-          placeholder="jane@bookeroo.com"
-          type="email"
-        />
-
-        <label>Full Name:</label>
-        <Field id="fullName" name="fullName" placeholder="Jane Doe" />
-
-        <label>Password:</label>
-        <Field id="password" name="password" />
-
-        <label>Confirm Password:</label>
-        <Field id="confirmPassword" name="confirmPassword" />
-
-        <div id="my-radio-group">I am a</div>
-        <div role="group" aria-labelledby="my-radio-group">
-          <label>
-            <Field type="radio" name="role" value="P" />
-            Publisher
-          </label>
-          <label>
-            <Field type="radio" name="role" value="SO" />
-            Shop owner
-          </label>
-          <label>
-            <Field type="radio" name="role" value="A" />
-            Admin
-          </label>
+    <div className="background">
+      <div className="form">
+        <div className="picture-slogan">
+          <p className="slogan">Your best bookstore in the <span className="catchy-word">town</span></p>
+          <div className="logo-align"><img className="bookstore-logo" src={bookstore} alt="bookstore"/></div>
         </div>
-        
-        <Button className="submitBtn" type="submit">Submit</Button>
+        <div className="registration-form">
+          <div className="logo-align"><img className="logo" src={logo} alt="logo"/></div>
+          <Formik
+            initialValues={{
+              username: '',
+              email: '',
+              fullName: '',
+              password: '',
+              confirmPassword: '',
+              role: ''
+            }}
+            onSubmit = {
+              async (values) => {
+                onSubmit(values);
+              }
+            }
+          >
+            <Form>
+              <div className="inputFields">
+                <div>
+                  <div className="align-label-and-field">
+                    <label className="label">User Name:</label>
+                    <Field className="fields" id="username" name="username" placeholder="Jane" />
+                  </div>
 
-      </Form>
-    </Formik>
+                  <div className="align-label-and-field">
+                    <label className="label">Email:</label>
+                    <Field
+                      className="fields"
+                      id="email"
+                      name="email"
+                      placeholder="jane@bookeroo.com"
+                      type="email"
+                    />
+                  </div>
+
+                  <div className="align-label-and-field">
+                    <label className="label">Full Name:</label>
+                    <Field className="fields" id="fullName" name="fullName" placeholder="Jane Doe" />
+                  </div>
+
+                  <div className="align-label-and-field">
+                    <label className="label">Password:</label>
+                    <Field className="fields" id="password" name="password" />
+                  </div>
+
+                  <div className="align-label-and-field">
+                    <label className="label">Confirm Password:</label>
+                    <Field className="fields" id="confirmPassword" name="confirmPassword" />
+                  </div>
+
+                  <div className="align-label-and-field">
+                    <div id="my-radio-group" className="label">I am a:</div>
+                    <div role="group" aria-labelledby="my-radio-group">
+                      <label className="label-radio">
+                        <Field type="radio" name="role" value="PU" />
+                        Public User
+                      </label>
+                      <label className="label-radio">
+                        <Field type="radio" name="role" value="P" />
+                        Publisher
+                      </label>
+                      <label className="label-radio">
+                        <Field type="radio" name="role" value="SO" />
+                        Shop owner
+                      </label>
+                    </div>
+                  </div>
+                </div>
+      
+                <div className="alignBtn">
+                  <Button 
+                    className="submitBtn" 
+                    style={{ 
+                      border: '1px solid #06f', 
+                      borderRadius:'3%',
+                      outline: 'none'
+                    }} 
+                    type="submit"
+                  >
+                    <span className="btn-text">Signup</span>
+                  </Button>
+                </div>
+                <div className="end-quote">Shop your favourite books, all in one place</div>
+              </div>
+
+            </Form>
+          </Formik>
+        </div>
+      </div>
+    </div>
   );
 }
 export default Register;

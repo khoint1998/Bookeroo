ed0f1e89c7490e8996ce0b43c98d8ba30ecf05f4
DerrenChinWeiHong <s3814934@student.rmit.edu.au>
2021-08-26 00:04:14 +0800

    Added Profile page and updated App.js with profile

--- FrontEnd/myfirstapp/src/App.js
+++ FrontEnd/myfirstapp/src/App.js
@@ -11,6 +11,7 @@ import store from "./store";
 import Landing from "./components/Layout/Landing";
 import Register from "./components/UserManagement/Register";
 import Login from "./components/UserManagement/Login";
+import Profile from "./components/Profile/Profile";
 
 const App = () => {
   return (
@@ -31,6 +32,7 @@ const App = () => {
           }
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/addPerson" component={AddPerson} />
+          <Route exact path="/profile" component={Profile} />
         
         </div>
       </Router>

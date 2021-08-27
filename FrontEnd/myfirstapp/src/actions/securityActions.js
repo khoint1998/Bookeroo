b8c86b966e9e344ec3be0afe463fef97153a4df6
Khoi <s3678755@rmit.edu.vn>
2021-08-27 10:49:00 +0700

    Updated Homy code to the latest ver

--- FrontEnd/myfirstapp/src/actions/securityActions.js
+++ FrontEnd/myfirstapp/src/actions/securityActions.js
@@ -25,23 +25,35 @@ export const createNewUser = (newUser, history) => async dispatch => {
 
 export const login = LoginRequest => async dispatch => {
     try {
-
-        //post => login request
-
-        //extract token from res.data
-
-        //set our token in the local storage
-
-        // set our token in header 
-
-        //decode the token on React
-
+        // post => Login Request
+        const res = await axios.post("/api/users/login", LoginRequest);
+        // extract token from res.data
+        const { token } = res.data;
+        // store the token in the localStorage
+        localStorage.setItem("jwtToken", token);
+        // set our token in header ***
+        setJWTToken(token);
+        // decode token on React
+        const decoded = jwt_decode(token);
         // dispatch to our securityReducer
-
+        dispatch({
+        type: SET_CURRENT_USER,
+        payload: decoded
+        });
     }
-    catch (err)
-    {
-
+    catch (err) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        });
     }
-
 }
+
+export const logout = () => dispatch => {
+    localStorage.removeItem("jwtToken");
+    setJWTToken(false);
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: {}
+    });
+  };

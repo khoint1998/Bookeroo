945de126cb942ec4a693f4c8b26682f007a7cf7c
Khoi <s3678755@rmit.edu.vn>
2021-09-28 16:52:36 +0700

    [new/chore] AWS CD: Finished setup all endpoints for AWS ECS

--- BackEnd/bookservices/src/main/java/com/rmit/sept/bk_bookservices/web/CopyController.java
+++ BackEnd/bookservices/src/main/java/com/rmit/sept/bk_bookservices/web/CopyController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000","http://front-153244410.ap-southeast-1.elb.amazonaws.com"})
 @RequestMapping("/bookeroo/copys")
 public class CopyController {
 
--- BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/web/UserController.java
+++ BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/web/UserController.java
@@ -28,7 +28,7 @@ import static com.rmit.sept.bk_loginservices.security.SecurityConstant.TOKEN_PRE
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000","http://front-153244410.ap-southeast-1.elb.amazonaws.com"})
 @RequestMapping("/bookeroo/users")
 public class UserController {
 
@@ -40,6 +40,11 @@ public class UserController {
 
     @Autowired
     private UserValidator userValidator;
+
+    @GetMapping("/")
+    public ResponseEntity<String> awsHealthCheck() {
+        return new ResponseEntity<String>("Status: OK", HttpStatus.OK);
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){

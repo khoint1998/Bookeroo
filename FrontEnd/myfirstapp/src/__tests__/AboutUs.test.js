f83f5d4fa38eb5da2d224df94a61498ab5100f3b
Khoi <s3678755@rmit.edu.vn>
2021-10-19 17:39:11 +0700

    [fix] test folder: fixed the path inside each test

--- FrontEnd/myfirstapp/src/__tests__/AboutUs.test.js
+++ FrontEnd/myfirstapp/src/__tests__/AboutUs.test.js
@@ -1,6 +1,6 @@
 import {render} from '@testing-library/react';
 import React from 'react';
-import AboutUs from '../src/components/AboutUs/AboutUs';
+import AboutUs from '../components/AboutUs/AboutUs';
 
 test('Render_text', () => {
     const {getByText} = render(<AboutUs/>);
